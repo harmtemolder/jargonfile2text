637f9f9fb24887093f7cb8a6797701e937713b03
Harm te Molder <mail@harmtemolder.com>
2024-11-26 16:06:56 +0100

    Parse with markdownify

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # jargonfile2text
 
 A Python script that turns [The Jargon File](https://github.com/agiacalone/jargonfile) into plain text.
+
+```shell
+pymarkdown --log-level INFO fix md/**/*.md
+```
--- jargonfile2text.py
+++ jargonfile2text.py
@@ -1,34 +1,88 @@
 #!/usr/bin/env python3
 
-
-import requests
-import zipfile
-import io
-
 from pathlib import Path
+from bs4 import BeautifulSoup
+from markdownify import chomp, ASTERISK, MarkdownConverter
+import shutil
+from markdownify import MarkdownConverter
+import re
 
 
-def download(repo):
-    url = f'https://github.com/{repo}/zipball/main'
-    print(f'Downloading {url}...')
-    r = requests.get(url, stream=True)
+class JargonFileConverter(MarkdownConverter):
+    def convert_a(self, el, text, convert_as_inline):
+        prefix, suffix, text = chomp(text)
+        if not text:
+            return ''
+        href = el.get('href')
 
-    if not r.ok:
-        raise ConnectionError
+        # Link to Markdown files instead
+        href = re.sub('\\.html', '.md', href)
 
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall('in')
+        title = el.get('title')
+        # For the replacement see #29: text nodes underscores are escaped
+        if (self.options['autolinks']
+                and text.replace(r'\_', '_') == href
+                and not title
+                and not self.options['default_title']):
+            # Shortcut syntax
+            return '<%s>' % href
+        if self.options['default_title'] and not title:
+            title = href
+        title_part = ' "%s"' % title.replace('"', r'\"') if title else ''
+        return '%s[%s](%s%s)%s' % (prefix, text, href, title_part, suffix) if href else text
 
 
-def main(repo, path):
-    if not Path('in').is_dir():
-        download(repo)
+def md(soup, **options):
+    return JargonFileConverter(**options).convert_soup(soup)
 
-    pass
+
+def to_md(f, soup):
+    markdown = md(
+        soup,
+        heading_style='ATX',
+        bullets='-',
+        strong_em_symbol=ASTERISK,
+        wrap=True,
+        wrap_width=80
+    )
+
+    md_folder = Path(re.sub('jargonfile/html', 'md', str(f.parent)))
+    md_path = md_folder / Path(f'{f.stem}.md')
+
+    print(f'to {md_path}')
+
+    with md_path.open('w') as out:
+        out.write(markdown)
+
+
+def clean():
+    shutil.rmtree(Path('md'))
+    Path('md').mkdir()
+
+
+def main():
+    clean()
+    html_path = Path('jargonfile/html')
+
+    for f in html_path.rglob('*'):
+        if f.is_dir():
+            continue
+
+        print(f'From {f}', end=' ')
+
+        if f.suffix == '.html':
+            html = f.read_text()
+            soup = BeautifulSoup(html, features='xml')
+            to_md(f, soup)
+        elif f.suffix == '.html~':
+            # Skip ISO-8859-1 encoded files
+            # TODO check if a UTF-8 encoded version exists
+            continue
+        elif f.suffix == '.css':
+            continue
+        else:
+            raise NotImplementedError
 
 
 if __name__ == '__main__':
-    main(
-        repo='agiacalone/jargonfile',
-        path='html'
-    )
+    main()
